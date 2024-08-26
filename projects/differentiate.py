a813d74b0ba5bd34a8346e6d1322fa1c4ee6eeac
Oskar-Idland <oskaridland@hotmail.com>
2024-08-26 19:36:59 +0200

    Passed test (problem  1.1 & 1.2)

--- projects/differentiate.py
+++ projects/differentiate.py
@@ -2,10 +2,26 @@ import numpy as np
 
 
 def differentiate(u, dt):
-    pass
+    N_t = len(u)
+    d_u = np.zeros(N_t)
+    
+    # Boundaries 
+    d_u[0] = (u[1] - u[0]) / dt
+    d_u[-1] = (u[-1] - u[-2]) / dt
+    
+    # Middle
+    for t_i in range(1, N_t-1):
+        d_u[t_i] = (u[t_i + 1] - u[t_i - 1]) / (2*dt)
+        
+    return d_u
 
 def differentiate_vector(u, dt):
-    pass
+    d_u = np.zeros(len(u))
+    d_u[0] = d_u[0] = (u[1] - u[0]) / dt
+    d_u[-1] = (u[-1] - u[-2]) / dt
+    d_u[1:-1] = (u[2:] - u[0:-2]) / (2*dt)
+    
+    return d_u
 
 def test_differentiate():
     t = np.linspace(0, 1, 10)
--- projects/mesh_function.py
+++ projects/mesh_function.py
@@ -2,10 +2,18 @@ import numpy as np
 
 
 def mesh_function(f, t):
-    pass
+    N_t = len(t)
+    y = np.zeros(N_t)
+    for i in range(N_t):
+        y[i] = f(t[i])
+    
+    return y
 
 def func(t):
-    pass
+    if 0 <= t and t <= 3:
+        return np.exp(-t)
+    elif 3 < t and t <= 4:
+        return np.exp(-3*t) 
 
 def test_mesh_function():
     t = np.array([1, 2, 3, 4])
